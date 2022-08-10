ca4536aae44eeb58421890c48e03ba93df3454f2
Anildeepsingh <anildeepsingh52@gmail.com>
2022-08-10 13:08:49 +0530

    header

--- todos-list/src/App.js
+++ todos-list/src/App.js
@@ -1,52 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./MyComponents/Header";
+import { TodoItem } from "./MyComponents/TodoItem";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
 
 function App() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Todo List
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  About
-                </a>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      <Header title="My Todos List" />
+      <Todos />
+      <Footer />
     </>
   );
 }
